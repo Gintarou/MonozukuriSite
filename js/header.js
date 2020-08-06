6b1ae58d5e8e1b239e3d50d167c36d73543653d3
rikuto <rikuto.10141219@gmail.com>
2020-08-06 15:31:05 +0900

    arrow function to nomal function

--- js/header.js
+++ js/header.js
@@ -4,16 +4,16 @@ const ham = document.getElementById('ham');
 const ham_menu_wrapper = document.getElementById('ham_menu_wrapper');
 const header = document.querySelector('header');
 
-ham.addEventListener('click', () => {
+ham.addEventListener('click', function() {
   ham.classList.toggle('clicked');
   ham_menu_wrapper.classList.toggle('clicked');
 });
 
-window.onload = () => {
+window.onload = function() {
   document.getElementById('home_first_heading').style.opacity = "1";
   document.getElementById('home_first_txt').style.opacity = "1";
 
-  setTimeout( () => {
+  setTimeout( function() {
     header.style.opacity = "1";
   }, 1500);
 }
